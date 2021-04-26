f56cb273f01949080b1d37ba4c53931fc2190364
Titus Wormer <tituswormer@gmail.com>
2021-04-26 16:56:04 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+/**
+ * @typedef {import('hast').Element & {tagName: 'h1'|'h2'|'h3'|'h4'|'h5'|'h6'|'hgroup'}} Heading
+ * @typedef {import('hast-util-is-element').AssertPredicate<Heading>} AssertHeading
+ */
+
 import {convertElement} from 'hast-util-is-element'
 
+/**
+ * Check if a node is a heading element (includes `hgroup`!).
+ * @type {AssertHeading}
+ */
+// @ts-ignore Sure, the assertion matches.
 export const heading = convertElement([
   'h1',
   'h2',
