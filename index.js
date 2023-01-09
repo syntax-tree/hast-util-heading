f68de5cf19e4cca891d1a981a72c4d1749419520
Titus Wormer <tituswormer@gmail.com>
2023-01-09 10:10:59 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 /**
- * @typedef {import('hast').Element & {tagName: 'h1'|'h2'|'h3'|'h4'|'h5'|'h6'|'hgroup'}} Heading
- * @typedef {import('hast-util-is-element').AssertPredicate<Heading>} AssertHeading
+ * @typedef {import('hast').Element} Element
  */
 
 import {convertElement} from 'hast-util-is-element'
 
 /**
  * Check if a node is a heading element (includes `hgroup`!).
- * @type {AssertHeading}
+ *
+ * @type {import('hast-util-is-element').AssertPredicate<Element & {tagName: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'hgroup'}>}
+ * @param node
+ *   Node to check.
+ * @returns
+ *   Whether node is a heading (`h1` through `h6`, and `hgroup`).
  */
 // @ts-expect-error Sure, the assertion matches.
 export const heading = convertElement([
