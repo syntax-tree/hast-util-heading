899b69cdd6232e8a512fc79175dc10df26cecf55
Titus Wormer <tituswormer@gmail.com>
2023-08-01 13:03:39 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
-import {heading} from './index.js'
+import {heading} from 'hast-util-heading'
 
 test('heading', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('hast-util-heading')).sort(), [
       'heading'
     ])
   })
