422cabe4ffa2dd81788f3363a4b9fa7b69ffd6b1
Titus Wormer <tituswormer@gmail.com>
2023-08-01 13:02:23 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,50 +1,60 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {heading} from './index.js'
-import * as mod from './index.js'
 
-test('heading', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['heading'],
-    'should expose the public api'
+test('heading', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'heading'
+    ])
+  })
+
+  await t.test('should return `false` without node', async function () {
+    // @ts-expect-error: check how a missing `node` is handled.
+    assert.equal(heading(), false)
+  })
+
+  await t.test('should return `false` with `null`', async function () {
+    assert.equal(heading(null), false)
+  })
+
+  await t.test(
+    'should return `false` when without `element`',
+    async function () {
+      assert.equal(heading({type: 'text'}), false)
+    }
   )
 
-  // @ts-expect-error: check how a missing `node` is handled.
-  assert.equal(heading(), false, 'should return `false` without node')
-
-  assert.equal(heading(null), false, 'should return `false` with `null`')
-
-  assert.equal(
-    heading({type: 'text'}),
-    false,
-    'should return `false` when without `element`'
+  await t.test(
+    'should return `false` when with invalid `element`',
+    async function () {
+      assert.equal(heading({type: 'element'}), false)
+    }
   )
 
-  assert.equal(
-    heading({type: 'element'}),
-    false,
-    'should return `false` when with invalid `element`'
+  await t.test(
+    'should return `false` when without not heading',
+    async function () {
+      assert.equal(
+        heading({
+          type: 'element',
+          tagName: 'a',
+          properties: {href: '#alpha', title: 'Bravo'},
+          children: [{type: 'text', value: 'Charlie'}]
+        }),
+        false
+      )
+    }
   )
 
-  assert.equal(
-    heading({
-      type: 'element',
-      tagName: 'a',
-      properties: {href: '#alpha', title: 'Bravo'},
-      children: [{type: 'text', value: 'Charlie'}]
-    }),
-    false,
-    'should return `false` when without not heading'
-  )
-
-  assert.equal(
-    heading({
-      type: 'element',
-      tagName: 'h1',
-      children: [{type: 'text', value: 'Delta'}]
-    }),
-    true,
-    'should return `true` when with heading'
-  )
+  await t.test('should return `true` when with heading', async function () {
+    assert.equal(
+      heading({
+        type: 'element',
+        tagName: 'h1',
+        children: [{type: 'text', value: 'Delta'}]
+      }),
+      true
+    )
+  })
 })
